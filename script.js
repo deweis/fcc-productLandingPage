9c5e0323bdd25f7fdda0907d245bd3050e3b1b43
Dennis <deweis@gmx.ch>
2018-07-10 11:28:23 +0200

    Add overlay menu

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('hamburger').addEventListener('click', function () {
+  document.getElementById('nav-toggled').style.display = 'flex';
+  document.getElementById('main').style.display = 'none';
+});
