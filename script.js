a8b39395e0b550772ef623f7ccc1180710095f5b
Dennis <deweis@gmx.ch>
2018-07-12 09:02:45 +0200

    Add toggler to overlay menu

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
 document.getElementById('hamburger').addEventListener('click', function () {
+  showOverlayMenu();
+});
+
+function showOverlayMenu() {
+  document.getElementById('header-claim').style.display = 'none';
   document.getElementById('nav-toggled').style.display = 'flex';
   document.getElementById('main').style.display = 'none';
-});
+  document.getElementById('one').classList.add('span-one');
+  document.getElementById('two').classList.add('span-two');
+  document.getElementById('three').classList.add('span-three');
+  document.getElementById('hamburger').addEventListener('click', function () {
+    hideOverlayMenu();
+  });
+}
+
+function hideOverlayMenu() {
+  document.getElementById('header-claim').style.display = 'block';
+  document.getElementById('nav-toggled').style.display = 'none';
+  document.getElementById('main').style.display = 'block';
+  document.getElementById('one').classList.remove('span-one');
+  document.getElementById('two').classList.remove('span-two');
+  document.getElementById('three').classList.remove('span-three');
+  document.getElementById('hamburger').addEventListener('click', function () {
+    showOverlayMenu();
+  });
+}
